e8c0679f76277ebef81be44bef2ac41ff588c328
Sonalika D <dsonalika10@gmail.com>
2024-09-01 23:02:02 +0530

    Feat: RetailNest - Your Perfect Store, Just a Click Away.

---
+++ front-end/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An E-Commerce website is an online platform..., and the global reach thry offer.</p>
+            <p>E-Commerse website typically displays</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
